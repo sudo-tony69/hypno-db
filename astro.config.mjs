a23a8a87277426e9cd34ca0e3700f2db3ff88dec
sudo-tony69 <anthroze69@gmail.com>
2026-09-21 21:10:16 +0000

    Config Modify

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://sudo-tony69.github.io/hypno-db',
+  site: 'https://sudo-tony69.github.io',
+  base: '/hypno-db',
 });
